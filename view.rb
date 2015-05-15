d1bfb2569ad5a748b81204856ad4a2045f7f6e01
Marc Cardacci <marc.cardacci@gmail.com>
2015-05-15 17:02:02 -0400

    adds aaaaahhhhju

--- view.rb
+++ view.rb
@@ -1,23 +1,33 @@
 
 module View
 
-  def get_input
+  def self.get_input
     gets.chomp
   end
 
-  def self.welcome_display
+  def self.welcome
+    system("clear")
+    puts "*" * 42
     puts "Welcome to Team Groovy's Discography App!"
+    puts "*" * 42
+    puts ""
+    puts ""
     sleep 0.8
     puts "What artist would you like to search for?"
   end
 
   def self.artist_members(members_array)
-    members_array.each {|member| puts member}
+    members_array.each {|member| puts member; sleep 0.02}
+    sleep 5.0
+
   end
 
   def self.display_profile(profile)
+    puts ""
     puts "Profile: #{profile}"
+    puts
     puts "What else would you like to view?"
+    puts ""
     self.artist_options
   end
 
@@ -26,18 +36,16 @@ module View
     puts "1. Members"
     puts "2. Number of Releases"
     puts "3. List Releases"
+    puts "4. Quit the Program"
   end
 
   def self.releases_options
     puts "Type the number corresponding to the album for more detail."
   end
 
-  def self.exit_menu
-    puts "Type exit to quit the program."
-  end
-
   def self.display(input)
     puts input
+    sleep 2.0
   end
 
 end
