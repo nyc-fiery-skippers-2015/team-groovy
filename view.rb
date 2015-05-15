7385751bfab43d56911a0471704ee5f0af8e57a0
Samantha Belkin & tekd <pair+sbelkin88.tekd@devbootcamp.com>
2015-05-15 16:05:27 -0400

    Edit view module

--- view.rb
+++ view.rb
@@ -8,20 +8,14 @@ module View
   def self.welcome_display
     puts "Welcome to Team Groovy's Discography App!"
     sleep 0.8
-    puts "What would you like to search by?"
-    self.menu
-  end
-
-  def self.menu
-    puts "1. Artist"
-    puts "2. Album"
+    puts "What artist would you like to search for?"
   end
 
   def self.artist_members(members_array)
     members_array.each {|member| puts member}
   end
 
-  def self.display_profile(profile)#string arg
+  def self.display_profile(profile)
     puts "Profile: #{profile}"
     puts "What else would you like to view?"
     self.artist_options
@@ -34,14 +28,16 @@ module View
     puts "3. List Releases"
   end
 
-  def self.album_options
-    puts "Please type the number for which option you'd like."
-    puts "1. Artist Name"
-    puts "2. Artist Label"
+  def self.releases_options
+    puts "Type the number corresponding to the album for more detail."
   end
 
   def self.exit_menu
     puts "Type exit to quit the program."
+  end
+
+  def self.display(input)
+    puts input
   end
 
 end
